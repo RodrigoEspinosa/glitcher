3fc5fb3ae1a7952d6d406cadceaa8391b0d823aa
Diego <diego2737@gmail.com>
2012-06-21 21:32:30 -0300

    recien me di cuenta que se podia achicar un poquito mas

--- glitcher.py
+++ glitcher.py
@@ -7,12 +7,6 @@ with open(outfile,"wb") as out:
 	for line in open(openFile, "rb"):
 		process = line
 		rand = random.randint(1,10000)
-		if (rand == 2):
-			process *= 2
-		elif(rand == 4):
-			process *= 4
-		elif(rand == 6):	
-			process *= 6
-		elif(rand == 8):
-			process *= 8
+		if (rand in [2,4,6,8]):
+			process *= rand
 		out.write(process)
